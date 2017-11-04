86859fda3d8580191759a9f017fc7131e3f356f2
Pratik Bhusal <PratikBhusal@users.noreply.github.com>
2017-11-04 15:28:06 -0500

    Moved into lift namespace

--- lexical_cast.hpp
+++ lexical_cast.hpp
@@ -1,38 +1,41 @@
-#ifndef LEXICAL_CAST_H
-#define LEXICAL_CAST_H
+#ifndef LIFT_LEXICAL_CAST_INCLUDED
+#define LIFT_LEXICAL_CAST_INCLUDED
 
-template <typename T> T lexical_cast();
-template <typename T> T lexical_cast(const std::string&);
-template <typename T> T lexical_cast(const T&, const T&);
-
-template <typename T> T lexical_cast()
+namespace lift
 {
-    std::cout << "Please enter a valid input: ";
-    std::string input;
-    std::cin >> input;
+    template <typename T> T lexical_cast();
+    template <typename T> T lexical_cast(const std::string&);
+    template <typename T> T lexical_cast(const T&, const T&);
 
-    return lexical_cast<T>(input);
+    template <typename T> T lexical_cast()
+    {
+        std::cout << "Please enter a valid input: ";
+        std::string input;
+        std::cin >> input;
+
+        return lexical_cast<T>(input);
+    }
+
+    template <typename T> T lexical_cast(const std::string& input)
+    {
+        std::istringstream parser(input);
+        T result;
+        parser >> result;
+
+        //  TODO: Why does parser.good() make it work? <2017-11-04, Pratik B.>
+        return ( parser.good() || parser.fail() ) ? lexical_cast<T>() : result;
+    }
+
+    template <typename T> T lexical_cast(const T& min, const T& max)
+    {
+        T result;
+
+        do {
+            result = lexical_cast<T>();
+        } while (result < min || result > max);
+
+        return result;
+    }
 }
 
-template <typename T> T lexical_cast(const std::string& input)
-{
-    std::istringstream parser(input);
-    T result;
-    parser >> result;
-
-    //  TODO: Figure out why we need parser.good() <2017-11-04, Pratik Bhusal>
-    return ( parser.good() || parser.fail() ) ? lexical_cast<T>() : result;
-}
-
-template <typename T> T lexical_cast(const T& min, const T& max)
-{
-    T result;
-
-    do {
-        result = lexical_cast<T>();
-    } while (result < min || result > max);
-
-    return result;
-}
-
-#endif
+#endif // LIFT_LEXICAL_CAST_INCLUDED
