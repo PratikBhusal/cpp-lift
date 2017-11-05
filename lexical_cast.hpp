e01c4615b1b068030ee10c6561c522e63621f021
Pratik Bhusal <PratikBhusal@users.noreply.github.com>
2017-11-05 10:37:19 -0600

    Changed lexical_cast to iterative function
    The main reason to do this is to (in theory) make it run more efficiently.

--- lexical_cast.hpp
+++ lexical_cast.hpp
@@ -1,27 +1,41 @@
 #ifndef LIFT_LEXICAL_CAST_INCLUDED
 #define LIFT_LEXICAL_CAST_INCLUDED
 
+#include <iostream>
+#include <sstream>
+#include <string>
+
 namespace lift
 {
     template <typename T> T lexical_cast();
-    template <typename T> T lexical_cast(const std::string&);
+    template <typename T> T lexical_cast(std::string);
     template <typename T> T lexical_cast(const T&, const T&);
 
     template <typename T> T lexical_cast() {
-        std::cout << "Please enter a valid input: ";
-        std::string input;
-        std::cin >> input;
-
-        return lexical_cast<T>(input);
+        return lexical_cast<T>( std::string() );
     }
 
-    template <typename T> T lexical_cast(const std::string& input) {
-        std::istringstream parser(input);
+    template <typename T> T lexical_cast(std::string input) {
+        std::stringstream parser(input);
         T result;
         parser >> result;
 
-        //  TODO: Why does parser.good() make it work? <2017-11-04, Pratik B.>
-        return ( parser.good() || parser.fail() ) ? lexical_cast<T>() : result;
+        // parser.good() for mixed valid and invalid characters. If removed, the
+        // input "1a2b" would not loop and would instead return 1.
+        while ( parser.fail() || parser.good() ) {
+            // input.clear(); // Do I need this?
+            parser.str( std::string() );
+            parser.clear();
+
+            std::cout << "Please enter a valid input: ";
+            std::cin >> input;
+            std::cin >> result;
+
+            parser.str(input);
+            parser >> result;
+        }
+
+        return result;
     }
 
     template <typename T> T lexical_cast(const T& min, const T& max) {
