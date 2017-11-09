8d930680d0f7f066934896a79b6fbc02b6685236
Pratik Bhusal <PratikBhusal@users.noreply.github.com>
2017-11-09 14:54:39 -0600

    Removed lexical_cast with 2 template parameters
    I don't really think this is too necessary. If I need to add it in the future, I can do so quite easily.

--- lexical_cast.hpp
+++ lexical_cast.hpp
@@ -9,7 +9,6 @@ namespace lift
 {
     template <typename T> T lexical_cast();
     template <typename T> T lexical_cast(std::string);
-    template <typename T> T lexical_cast(const T&, const T&);
 
     template <typename T> T lexical_cast() {
         return lexical_cast<T>( std::string() );
@@ -23,8 +22,6 @@ namespace lift
         // parser.good() for mixed valid and invalid characters. If removed, the
         // input "1a2b" would not loop and would instead return 1.
         while ( parser.fail() || parser.good() ) {
-            // input.clear(); // Do I need this?
-            parser.str( std::string() );
             parser.clear();
 
             std::cout << "Please enter a valid input: ";
@@ -33,16 +30,6 @@ namespace lift
             parser.str(input);
             parser >> result;
         }
-
-        return result;
-    }
-
-    template <typename T> T lexical_cast(const T& min, const T& max) {
-        T result;
-
-        do {
-            result = lexical_cast<T>();
-        } while (result < min || result > max);
 
         return result;
     }
